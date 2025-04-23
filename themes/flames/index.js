5298966d4c3a5fca9f46ce7e41e11b132c1f20bd
flames <Flamesking@msn.com>
2025-04-23 11:18:41 +0800

    ...

--- themes/flames/index.js
+++ themes/flames/index.js
@@ -43,7 +43,7 @@ import SideRight from './components/SideRight'
 import CONFIG from './config'
 import { Style } from './style'
 import AISummary from '@/components/AISummary'
-import UnderwaterBackground from './components/UnderwaterBackground.js'
+import UnderwaterBackground from './components/UnderwaterBackground'
 
 /**
  * 基础布局 采用上中下布局，移动端使用顶部侧边导航栏
